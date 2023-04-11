f0fb4569971f413cdf96b03b7fa8de89687d8ce5
Ricky Kuo <rickykuo925@gmail.com>
2023-04-11 22:15:18 +0100

    posts widget

--- services/index.js
+++ services/index.js
@@ -1,6 +1,6 @@
 import { request, gql } from "graphql-request";
 
-const graphqlApi = process.env.HYGRAPH_ENDPOINT || "";
+const graphqlApi = process.env.NEXT_PUBLIC_HYGRAPH_API_KEY || "";
 
 export const getPosts = async () => {
   const query = gql`
@@ -34,4 +34,46 @@ export const getPosts = async () => {
 
   const results = await request(graphqlApi, query);
   return results.postsConnection.edges;
+};
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      posts(last: 3, orderBy: createdAt_DESC) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlApi, query);
+  return result.posts;
+};
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlApi, query);
+
+  return result.posts;
 };
